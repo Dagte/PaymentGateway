9bcce7e2b0d8bb80e2c4526748e12a4e2184e586
David Miguel Gaite Cruz <david@onoffapp.com>
2026-02-09 23:25:59 +0200

    feat: change PaymentsRepository to support concurrency via usage of ConcurrentHashMap

--- src/main/java/com/checkout/payment/gateway/infrastructure/persistence/PaymentsRepository.java
+++ src/main/java/com/checkout/payment/gateway/infrastructure/persistence/PaymentsRepository.java
@@ -1,15 +1,15 @@
 package com.checkout.payment.gateway.infrastructure.persistence;
 
 import com.checkout.payment.gateway.core.model.Payment;
-import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PaymentsRepository {
 
-  private final HashMap<UUID, Payment> payments = new HashMap<>();
+  private final ConcurrentHashMap<UUID, Payment> payments = new ConcurrentHashMap<>();
 
   public void add(Payment payment) {
     payments.put(payment.getId(), payment);
@@ -19,4 +19,7 @@ public class PaymentsRepository {
     return Optional.ofNullable(payments.get(id));
   }
 
+  public int count() {
+    return payments.size();
+  }
 }
